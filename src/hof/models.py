4d6731dc3f3238d9282f8d2328669cd6cd91f5ba
pitachips <hjkim3323@gmail.com>
2016-02-18 02:31:33 +0900

    Store, StoreImage, Review, ReviewImage 모델 1차 생성

---
+++ src/hof/models.py
@@ -0,0 +1,44 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
+from onedayhof.utils import random_name_upload_to
+
+
+class Store(models.Model):
+
+    name = models.CharField(max_length=80)
+    owner = models.CharField(max_length=20)
+    contract_term = models.TextField()
+    tel = models.CharField(max_length=20)
+    address = models.CharField(max_length=120)
+    lnglat = models.CharField(max_length=80, default='')
+    max_guest = models.CharField(max_length=20)
+
+    menu = models.TextField(blank=True, default='')
+    rating = models.PositiveSmallIntegerField(blank=True, default=0)
+    description = models.TextField(blank=True, default='')
+
+
+    def __str__(self):
+        return self.name
+
+
+class StoreImage(models.Model):
+    store = models.ForeignKey(Store, default=None)
+    image = models.ImageField(upload_to=random_name_upload_to)
+
+
+
+class Review(models.Model):
+    store = models.ForeignKey(Store)
+    rating = models.PositiveSmallIntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
+
+
+class ReviewImage(models.Model):
+    review = models.ForeignKey(Review, default=None)
+    image = models.ImageField(blank=True, null=True, upload_to=random_name_upload_to)
---
+++ src/onedayhof/utils.py
@@ -0,0 +1,12 @@
+import os
+from django.utils import timezone
+from uuid import uuid4
+
+
+def random_name_upload_to(instance, filename):
+    app_label = instance.__class__._meta.app_label
+    cls_name = instance.__class__.__name__.lower()
+    ymd_path = timezone.now().strftime('%Y/%m/%d')
+    name = uuid4().hex
+    extension = os.path.splitext(filename)[-1].lower()
+    return os.path.join(app_label, cls_name, ymd_path, name + extension)
