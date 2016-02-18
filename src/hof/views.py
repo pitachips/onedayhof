206a88ccba6a145dd464ce751b97d247075dd729
pitachips <hjkim3323@gmail.com>
2016-02-18 11:47:37 +0900

    debug-toobar, bootstrap3 설치 및 multiple-image-formset upload 구현(store_new 뷰)

--- src/hof/views.py
+++ src/hof/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.forms import modelformset_factory
+
+from .models import Store, StoreImage
+from .forms import StoreForm, StoreImageForm
 
 
 def index(request):
@@ -13,4 +17,30 @@ def store_detail(request, pk):
     store = get_object_or_404(Store, pk=pk)
     return render(request, 'hof/store_detail.html', {
         'store':store,
+    })
+
+
+def store_new(request):
+    ImageFormSet = modelformset_factory(StoreImage, form=StoreImageForm, extra=3)
+
+    if request.method == 'POST':
+        storeForm = StoreForm(request.POST)
+        formset = ImageFormSet(request.POST, request.FILES, queryset=StoreImage.objects.none())
+        if storeForm.is_valid() and formset.is_valid():
+            store = storeForm.save(commit=False)
+            store.owner = request.user
+            store.save()
+            for form in formset.cleaned_data:
+                image = form['image']
+                photo = StoreImage(post=post_form, image=image)
+                photo.save()
+            messages.success(request, "성공적")
+            return redirect("/")
+    else:
+        storeForm = StoreForm()
+        formset = ImageFormSet(queryset=StoreImage.objects.none())
+
+    return render(request, 'hof/store_form.html', {
+        'storeForm': storeForm,
+        'formset': formset,
     })
--- src/onedayhof/settings.py
+++ src/onedayhof/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
+    'bootstrap3',
     'hof',
 ]
 
