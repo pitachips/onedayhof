aa04e24fe1215001ddf77e52d1bfb9caafed6fb5
pitachips <hjkim3323@gmail.com>
2016-02-24 20:22:33 +0900

    정리정돈 실패한듯

--- src/hof/urls.py
+++ src/hof/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/delete/$', views.store_delete, name='store_delete'),
 
     url(r'^(?P<store_id>[0-9]+)/review/new/$', views.review_new, name='review_new'),
-    url(r'^(?P<store_id>[0-9]+)/review/(?P<review_id>[0-9]+)/edit/$', views.review_edit, name='review_edit'),
+    # url(r'^(?P<store_id>[0-9]+)/review/(?P<review_id>[0-9]+)/edit/$', views.review_edit, name='review_edit'),
     url(r'^(?P<store_id>[0-9]+)/review/(?P<review_id>[0-9]+)/delete/$', views.review_delete, name='review_delete'),
 
     url(r'^(?P<store_id>[0-9]+)/favorite_this_store/(?P<flag>[01])/(?P<from_fav_list>[\w]+)/$', views.favorite_this_store, name='favorite_this_store'),
